33949511ae2064ad6dc0ae9d20ab53c92e2c4678
Anthony Alvarez <anthony.alvarez.galvez@gmail.com>
2026-09-09 19:38:35 -0700

    Fix tool video thumbnails on mobile

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ if (window.location.protocol === "file:") {
   `;
   document.body.prepend(warning);
 }
+
+document.querySelectorAll(".video-facade").forEach((facade) => {
+  facade.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const iframe = document.createElement("iframe");
+    iframe.src = `https://www.youtube.com/embed/${facade.dataset.videoId}?autoplay=1&rel=0&modestbranding=1&playsinline=1`;
+    iframe.title = facade.dataset.videoTitle;
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+    iframe.referrerPolicy = "strict-origin-when-cross-origin";
+    iframe.allowFullscreen = true;
+
+    facade.parentElement.replaceChildren(iframe);
+  });
+});
